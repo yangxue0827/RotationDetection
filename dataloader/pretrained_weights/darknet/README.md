f1a293ed677593e9ef5749a911c6fae247613237
yangxue <yangxue0827@126.com>
2020-11-10 23:09:10 +0800

    support darknet53

---
+++ dataloader/pretrained_weights/darknet/README.md
@@ -0,0 +1,31 @@
+1. Please download [resnet50_v1](http://download.tensorflow.org/models/resnet_v1_50_2016_08_28.tar.gz), [resnet101_v1](http://download.tensorflow.org/models/resnet_v1_101_2016_08_28.tar.gz), [resnet152_v1](http://download.tensorflow.org/models/resnet_v1_152_2016_08_28.tar.gz), [efficientnet](https://github.com/tensorflow/tpu/tree/master/models/official/efficientnet), [mobilenet_v2](https://storage.googleapis.com/mobilenet_v2/checkpoints/mobilenet_v2_1.0_224.tgz), darknet53 ([Baidu Drive (1jg2)](https://pan.baidu.com/s/1p8V9aaivo9LNxa_OjXjUwA), [Google Drive](https://drive.google.com/drive/folders/1zyg1bvdmLxNRIXOflo_YmJjNJdpHX2lJ?usp=sharing)) pre-trained models on Imagenet, put them to $PATH_ROOT/dataloader/pretrained_weights.       
+2. **(Recommend in this repo)** Or you can choose to use better backbones (resnet_v1d), refer to [gluon2TF](https://github.com/yangJirui/gluon2TF).    
+* [Baidu Drive (5ht9)](https://pan.baidu.com/s/1GpqKg0dOaaWmwshvv1qWGg)          
+* [Google Drive](https://drive.google.com/drive/folders/1BM8ffn1WnsRRb5RcuAcyJAHX8NS2M1Gz?usp=sharing)      
+3. Path tree of pretrained_weight 
+```
+├── pretrained_weight
+│   ├── darknet
+│       ├── checkpoint
+│       ├── darknet.ckpt.data-00000-of-00001
+│       ├── darknet.ckpt.index
+│       ├── darknet.ckpt.meta
+│   ├── efficientnet
+│       ├── efficientnet-b0
+│           ├── checkpoint
+│           ├── model.ckpt.data-00000-of-00001
+│           ├── model.ckpt.index
+│           ├── model.ckpt.meta
+│    ├── mobilenet
+│       ├── mobilenet_v1_0.25_128.ckpt.data-00000-of-00001
+│       ├── mobilenet_v1_0.25_128.ckpt.index
+│       ├── mobilenet_v1_0.25_128.ckpt.meta
+│       ├── mobilenet_v1_0.25_128.tflite
+│       ├── mobilenet_v1_0.25_128_eval.pbtxt
+│       ├── mobilenet_v1_0.25_128_frozen.pb
+│       ├── mobilenet_v1_0.25_128_info.txt
+│    ├── resnet_v1_50.ckpt    
+│    ├── resnet50_v1d.ckpt.index    
+│    ├── resnet50_v1d.ckpt.data-00000-of-00001    
+│    ├── resnet50_v1d.ckpt.meta    
+```  
---
+++ libs/configs/DOTA/retinanet/cfgs_darknet_dota_v1.py
@@ -0,0 +1,128 @@
+# -*- coding: utf-8 -*-
+from __future__ import division, print_function, absolute_import
+import os
+import tensorflow as tf
+import math
+
+"""
+RetinaNet-H
+FLOPs: 974501532;    Trainable params: 49130388
+This is your result for task 1:
+
+mAP: 0.6444964295117241
+ap of each class: plane:0.8901867511103236, baseball-diamond:0.7012686597492714, bridge:0.4228891221408001, ground-track-field:0.6397414936828507, small-vehicle:0.6825617995113188, large-vehicle:0.5246615044986218, ship:0.733647839712775, tennis-court:0.9081283665155168, basketball-court:0.7378951221636981, storage-tank:0.7582823106802029, soccer-ball-field:0.40800259905241326, roundabout:0.5626065355351864, harbor:0.5339872781465107, swimming-pool:0.6763786762737724, helicopter:0.48720838390259885
+The submitted information is :
+
+Description: RetinaNet_DOTA_2x_20201109_70.2w
+Username: yangxue
+Institute: DetectionTeamUCAS
+Emailadress: yangxue16@mails.ucas.ac.cn
+TeamMembers: yangxue, yangjirui
+
+"""
+
+# ------------------------------------------------
+VERSION = 'RetinaNet_DOTA_2x_20201109'
+NET_NAME = 'darknet'  # 'MobilenetV2'
+
+# ---------------------------------------- System
+ROOT_PATH = os.path.abspath('../../')
+print(20*"++--")
+print(ROOT_PATH)
+GPU_GROUP = "2,3"
+NUM_GPU = len(GPU_GROUP.strip().split(','))
+SHOW_TRAIN_INFO_INTE = 20
+SMRY_ITER = 200
+SAVE_WEIGHTS_INTE = 27000 * 2
+
+SUMMARY_PATH = ROOT_PATH + '/output/summary'
+TEST_SAVE_PATH = ROOT_PATH + '/utils/test_result'
+
+if NET_NAME.startswith("resnet"):
+    weights_name = NET_NAME
+elif NET_NAME.startswith("MobilenetV2"):
+    weights_name = "mobilenet/mobilenet_v2_1.0_224"
+elif NET_NAME.startswith("darknet"):
+    weights_name = "darknet/darknet"
+else:
+    raise Exception('net name must in [resnet, MobilenetV2, darknet]')
+
+PRETRAINED_CKPT = ROOT_PATH + '/dataloader/pretrained_weights/' + weights_name + '.ckpt'
+TRAINED_CKPT = os.path.join(ROOT_PATH, 'output/trained_weights')
+EVALUATE_DIR = ROOT_PATH + '/output/evaluate_result_pickle/'
+
+# ------------------------------------------ Train and Test
+RESTORE_FROM_RPN = False
+FIXED_BLOCKS = 1  # allow 0~3
+FREEZE_BLOCKS = [True, False, False, False, False]  # for gluoncv backbone
+USE_07_METRIC = True
+ADD_BOX_IN_TENSORBOARD = True
+
+MUTILPY_BIAS_GRADIENT = 2.0  # if None, will not multipy
+GRADIENT_CLIPPING_BY_NORM = 10.0  # if None, will not clip
+
+CLS_WEIGHT = 1.0
+REG_WEIGHT = 1.0 / 5.0
+REG_LOSS_MODE = None
+ALPHA = 1.0
+BETA = 1.0
+
+BATCH_SIZE = 1
+EPSILON = 1e-5
+MOMENTUM = 0.9
+LR = 5e-4
+DECAY_STEP = [SAVE_WEIGHTS_INTE*12, SAVE_WEIGHTS_INTE*16, SAVE_WEIGHTS_INTE*20]
+MAX_ITERATION = SAVE_WEIGHTS_INTE*20
+WARM_SETP = int(1.0 / 4.0 * SAVE_WEIGHTS_INTE)
+
+# -------------------------------------------- Dataset
+DATASET_NAME = 'DOTA'  # 'pascal', 'coco'
+PIXEL_MEAN = [123.68, 116.779, 103.939]  # R, G, B. In tf, channel is RGB. In openCV, channel is BGR
+PIXEL_MEAN_ = [0.485, 0.456, 0.406]
+PIXEL_STD = [0.229, 0.224, 0.225]  # R, G, B. In tf, channel is RGB. In openCV, channel is BGR
+IMG_SHORT_SIDE_LEN = 832
+IMG_MAX_LENGTH = 832
+CLASS_NUM = 15
+
+IMG_ROTATE = False
+RGB2GRAY = False
+VERTICAL_FLIP = False
+HORIZONTAL_FLIP = True
+IMAGE_PYRAMID = False
+
+# --------------------------------------------- Network
+SUBNETS_WEIGHTS_INITIALIZER = tf.random_normal_initializer(mean=0.0, stddev=0.01, seed=None)
+SUBNETS_BIAS_INITIALIZER = tf.constant_initializer(value=0.0)
+PROBABILITY = 0.01
+FINAL_CONV_BIAS_INITIALIZER = tf.constant_initializer(value=-math.log((1.0 - PROBABILITY) / PROBABILITY))
+WEIGHT_DECAY = 1e-4
+USE_GN = False
+FPN_CHANNEL = 256
+NUM_SUBNET_CONV = 4
+FPN_MODE = 'fpn'
+
+# --------------------------------------------- Anchor
+LEVEL = ['P3', 'P4', 'P5', 'P6', 'P7']
+BASE_ANCHOR_SIZE_LIST = [32, 64, 128, 256, 512]
+ANCHOR_STRIDE = [8, 16, 32, 64, 128]
+ANCHOR_SCALES = [2 ** 0, 2 ** (1.0 / 3.0), 2 ** (2.0 / 3.0)]
+ANCHOR_RATIOS = [1, 1 / 3, 3.]
+ANCHOR_ANGLES = [-90, -75, -60, -45, -30, -15]
+ANCHOR_SCALE_FACTORS = None
+USE_CENTER_OFFSET = True
+METHOD = 'H'
+USE_ANGLE_COND = False
+ANGLE_RANGE = 90  # or 180
+
+# -------------------------------------------- Head
+SHARE_NET = True
+USE_P5 = True
+IOU_POSITIVE_THRESHOLD = 0.5
+IOU_NEGATIVE_THRESHOLD = 0.4
+
+NMS = True
+NMS_IOU_THRESHOLD = 0.1
+MAXIMUM_DETECTIONS = 100
+FILTERED_SCORE = 0.05
+VIS_SCORE = 0.4
+
