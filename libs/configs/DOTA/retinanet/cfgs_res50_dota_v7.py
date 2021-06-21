05be9852573ad61b5b1b686ebd924e9351580581
yangxue0827 <yangxue0827@126.com>
2021-06-21 13:50:18 +0800

    fix #23

--- libs/configs/DOTA/retinanet/cfgs_res50_dota_v7.py
+++ libs/configs/DOTA/retinanet/cfgs_res50_dota_v7.py
@@ -10,14 +10,12 @@ from dataloader.pretrained_weights.pretrain_zoo import PretrainModelZoo
 
 # schedule
 BATCH_SIZE = 1
-GPU_GROUP = "0,1,2,3"
+GPU_GROUP = "0"
 NUM_GPU = len(GPU_GROUP.strip().split(','))
-SAVE_WEIGHTS_INTE = 27000 * 2
+SAVE_WEIGHTS_INTE = 27000
 DECAY_STEP = np.array(DECAY_EPOCH, np.int32) * SAVE_WEIGHTS_INTE
 MAX_ITERATION = SAVE_WEIGHTS_INTE * MAX_EPOCH
 WARM_SETP = int(WARM_EPOCH * SAVE_WEIGHTS_INTE)
-
-# dataset
 
 # model
 # backbone
@@ -27,17 +25,44 @@ TRAINED_CKPT = os.path.join(ROOT_PATH, 'output/trained_weights')
 
 # bbox head
 METHOD = 'R'
+ANCHOR_RATIOS = [1, 1 / 3., 3.]
 
 # loss
 CLS_WEIGHT = 1.0
 REG_WEIGHT = 1.0 / 5.0
 REG_LOSS_MODE = None
 
-VERSION = 'RetinaNet_DOTA_2x_20210505'
+VERSION = 'RetinaNet_DOTA_1x_20210610'
 
 """
 RetinaNet-R + 90
-FLOPs: 1266042163;    Trainable params: 37843416
+FLOPs: 512292465;    Trainable params: 34524216
+This is your result for task 1:
 
+    mAP: 0.6725084704054188
+    ap of each class:
+    plane:0.8928344652701296,
+    baseball-diamond:0.7304554873104306,
+    bridge:0.34861749965443384,
+    ground-track-field:0.6454757711323661,
+    small-vehicle:0.7358328451641203,
+    large-vehicle:0.7333202044070867,
+    ship:0.829537451899966,
+    tennis-court:0.8991725822927377,
+    basketball-court:0.7414258616115545,
+    storage-tank:0.7907933807503915,
+    soccer-ball-field:0.5718970329039421,
+    roundabout:0.5956995083477648,
+    harbor:0.51027563083865,
+    swimming-pool:0.6614448075148596,
+    helicopter:0.4008445269828484
+
+The submitted information is :
+
+Description: RetinaNet_DOTA_1x_20210610_35.1w
+Username: SJTU-Det
+Institute: SJTU
+Emailadress: yangxue-2019-sjtu@sjtu.edu.cn
+TeamMembers: yangxue
 """
 
