13b76fe6d7a7b2cb9cb4e25f838d93e53c14c117
yangxue0827 <yangxue0827@126.com>
2021-06-24 17:45:04 +0800

    support probiou and update fcos-rsdet

--- libs/models/losses/losses_gwd.py
+++ libs/models/losses/losses_gwd.py
@@ -105,3 +105,92 @@ class LossGWD(Loss):
         normalizer = tf.maximum(1.0, normalizer)
 
         return tf.reduce_sum(wasserstein_loss) / normalizer
+
+    def probiou(self, mode, preds, anchor_state, target_boxes, anchors,
+                is_refine=False):
+        if self.cfgs.METHOD == 'H' and not is_refine:
+            x_c = (anchors[:, 2] + anchors[:, 0]) / 2
+            y_c = (anchors[:, 3] + anchors[:, 1]) / 2
+            h = anchors[:, 2] - anchors[:, 0] + 1
+            w = anchors[:, 3] - anchors[:, 1] + 1
+            theta = -90 * tf.ones_like(x_c)
+            anchors = tf.transpose(tf.stack([x_c, y_c, w, h, theta]))
+
+        indices = tf.reshape(tf.where(tf.equal(anchor_state, 1)), [-1, ])
+
+        preds = tf.gather(preds, indices)
+        target_boxes = tf.gather(target_boxes, indices)
+        anchors = tf.gather(anchors, indices)
+
+        target_boxes_ = tf.reshape(target_boxes[:, :-1], [-1, 5])
+
+        if self.cfgs.ANGLE_RANGE == 180:
+            anchors = tf.py_func(coordinate_present_convert,
+                                 inp=[anchors, -1, False],
+                                 Tout=tf.float32)
+            anchors = tf.reshape(anchors, [-1, 5])
+            target_boxes_ = tf.py_func(coordinate_present_convert,
+                                       inp=[target_boxes_, -1, False],
+                                       Tout=tf.float32)
+            target_boxes_ = tf.reshape(target_boxes_, [-1, 5])
+
+        boxes_pred = bbox_transform.rbbox_transform_inv(boxes=anchors, deltas=preds)
+        boxes_pred = tf.reshape(boxes_pred, [-1, 5])
+
+        x1, y1, w1, h1, theta1 = tf.unstack(boxes_pred, axis=1)
+        x2, y2, w2, h2, theta2 = tf.unstack(target_boxes_, axis=1)
+        x1 = tf.reshape(x1, [-1, 1])
+        y1 = tf.reshape(y1, [-1, 1])
+        h1 = tf.reshape(h1, [-1, 1])
+        w1 = tf.reshape(w1, [-1, 1])
+        theta1 = tf.reshape(theta1, [-1, 1])
+        x2 = tf.reshape(x2, [-1, 1])
+        y2 = tf.reshape(y2, [-1, 1])
+        h2 = tf.reshape(h2, [-1, 1])
+        w2 = tf.reshape(w2, [-1, 1])
+        theta2 = tf.reshape(theta2, [-1, 1])
+        theta1 *= np.pi / 180
+        theta2 *= np.pi / 180
+
+        # convert values to the probIoU
+        aa = w1 ** 2 / 12
+        bb = h1 ** 2 / 12
+        angles = theta1
+        a1 = aa * tf.math.pow(tf.math.cos(angles), 2.) + bb * tf.math.pow(tf.math.sin(angles), 2.)
+        b1 = aa * tf.math.pow(tf.math.sin(angles), 2.) + bb * tf.math.pow(tf.math.cos(angles), 2.)
+        c1 = 0.5 * (aa - bb) * tf.math.sin(2. * angles)
+
+        aa = w2 ** 2 / 12
+        bb = h2 ** 2 / 12
+        angles = theta2
+        a2 = aa * tf.math.pow(tf.math.cos(angles), 2.) + bb * tf.math.pow(tf.math.sin(angles), 2.)
+        b2 = aa * tf.math.pow(tf.math.sin(angles), 2.) + bb * tf.math.pow(tf.math.cos(angles), 2.)
+        c2 = 0.5 * (aa - bb) * tf.math.sin(2. * angles)
+
+        # compute probIoU
+        B1 = 1 / 4. * ((a1 + a2) * (y1 - y2) ** 2. + (b1 + b2) * (x1 - x2) ** 2.) + 1 / 2. * (
+        (c1 + c2) * (x2 - x1) * (y1 - y2))
+        B1 /= ((a1 + a2) * (b1 + b2) - (c1 + c2) ** 2. + 1e-3)
+
+        sqrt = (a1 * b1 - c1 ** 2) * (a2 * b2 - c2 ** 2)
+        sqrt = tf.clip_by_value(sqrt, 1e-3, tf.reduce_max(sqrt) + 1e-3)
+        B2 = ((a1 + a2) * (b1 + b2) - (c1 + c2) ** 2.) / (4. * tf.math.sqrt(sqrt) + 1e-3)
+        B2 = tf.clip_by_value(B2, 1e-3, tf.reduce_max(B2) + 1e-3)
+        B2 = 1 / 2. * tf.math.log(B2)
+
+        Bd = B1 + B2
+        Bd = tf.clip_by_value(Bd, 1e-3, 100.)
+
+        l1 = tf.math.sqrt(1 - tf.math.exp(-Bd) + 1e-3)
+
+        if mode == 'l3':
+            l2 = tf.math.pow(l1, 2.)
+            probiou = - tf.math.log(1. - l2 + 1e-3)
+        else:
+            probiou = l1
+
+        normalizer = tf.stop_gradient(tf.where(tf.equal(anchor_state, 1)))
+        normalizer = tf.cast(tf.shape(normalizer)[0], tf.float32)
+        normalizer = tf.maximum(1.0, normalizer)
+
+        return tf.reduce_sum(probiou) / normalizer
--- libs/models/losses/losses_gwd_pytorch.py
+++ libs/models/losses/losses_gwd_pytorch.py
@@ -1,13 +1,11 @@
-# --------------------------------------------------------
-# gwd pytorch version
-# Licensed under The Apache-2.0 License [see LICENSE for details]
-# Written by Yue Zhou and Xue Yang
-# --------------------------------------------------------
-
 import torch
 import torch.nn as nn
 from torch.autograd import Variable
 import numpy as np
+
+
+# from ..builder import LOSSES
+# from .utils import weighted_loss
 
 
 def trace(A):
@@ -17,9 +15,8 @@ def trace(A):
 def sqrt_newton_schulz_autograd(A, numIters, dtype):
     batchSize = A.data.shape[0]
     dim = A.data.shape[1]
-    # print(batchSize, dim)
     normA = A.mul(A).sum(dim=1).sum(dim=1).sqrt()
-    Y = A.div(normA.view(batchSize, 1, 1).expand_as(A));
+    Y = A.div(normA.view(batchSize, 1, 1).expand_as(A))
     I = Variable(torch.eye(dim, dim).view(1, dim, dim).
                  repeat(batchSize, 1, 1).type(dtype), requires_grad=False)
     Z = Variable(torch.eye(dim, dim).view(1, dim, dim).
@@ -34,23 +31,29 @@ def sqrt_newton_schulz_autograd(A, numIters, dtype):
 
 
 def wasserstein_distance_sigma(sigma1, sigma2):
-    print('a1:',torch.matmul(sigma1, sigma1) + torch.matmul(sigma2,sigma2))
-    print('a2:',2 * sqrt_newton_schulz_autograd(torch.matmul(torch.matmul(sigma1, torch.matmul(sigma2, sigma2)), sigma1), 20, torch.FloatTensor))
-    wasserstein_distance_item2 = torch.matmul(sigma1, sigma1) + torch.matmul(sigma2,sigma2) - 2 * sqrt_newton_schulz_autograd(torch.matmul(torch.matmul(sigma1, torch.matmul(sigma2, sigma2)), sigma1), 10, torch.FloatTensor)
-    print('w2',wasserstein_distance_item2)
+    wasserstein_distance_item2 = torch.matmul(sigma1, sigma1) + torch.matmul(sigma2,
+                                                                             sigma2) - 2 * sqrt_newton_schulz_autograd(
+        torch.matmul(torch.matmul(sigma1, torch.matmul(sigma2, sigma2)), sigma1), 20, torch.FloatTensor)
     wasserstein_distance_item2 = trace(wasserstein_distance_item2)
-    print('trace',wasserstein_distance_item2)
 
     return wasserstein_distance_item2
 
 
 # @weighted_loss
-def gwd_loss(pred, target, weight, eps=1e-6):
-    """gwd loss.
+def gwds_loss(pred, target, weight, eps=1e-6):
+    """IoU loss.
 
-    Computing the gwd loss between a set of predicted bboxes and target bboxes.
+    Computing the IoU loss between a set of predicted bboxes and target bboxes.
+    The loss is calculated as negative log of IoU.
 
-    The weight of the gwd loss (pytorch version) is more appropriate between 5-10 (for reference only)
+    Args:
+        pred (Tensor): Predicted bboxes of format (xc, yc, w, h, a),
+            shape (n, 5).
+        target (Tensor): Corresponding gt bboxes, shape (n, 5).
+        eps (float): Eps to avoid log(0).
+
+    Return:
+        Tensor: Loss tensor.
     """
     mask = (weight > 0).detach()
     pred = pred[mask]
@@ -61,12 +64,14 @@ def gwd_loss(pred, target, weight, eps=1e-6):
     w1 = pred[:, 2]
     h1 = pred[:, 3]
     theta1 = pred[:, 4]
-    
+
     sigma1_1 = w1 / 2 * torch.cos(theta1) ** 2 + h1 / 2 * torch.sin(theta1) ** 2
     sigma1_2 = w1 / 2 * torch.sin(theta1) * torch.cos(theta1) - h1 / 2 * torch.sin(theta1) * torch.cos(theta1)
     sigma1_3 = w1 / 2 * torch.sin(theta1) * torch.cos(theta1) - h1 / 2 * torch.sin(theta1) * torch.cos(theta1)
     sigma1_4 = w1 / 2 * torch.sin(theta1) ** 2 + h1 / 2 * torch.cos(theta1) ** 2
-    sigma1 = torch.reshape(torch.cat((sigma1_1.unsqueeze(1), sigma1_2.unsqueeze(1), sigma1_3.unsqueeze(1), sigma1_4.unsqueeze(1)), axis=1), (-1, 2, 2))
+    sigma1 = torch.reshape(
+        torch.cat((sigma1_1.unsqueeze(1), sigma1_2.unsqueeze(1), sigma1_3.unsqueeze(1), sigma1_4.unsqueeze(1)), axis=1),
+        (-1, 2, 2))
 
     x2 = target[:, 0]
     y2 = target[:, 1]
@@ -77,65 +82,154 @@ def gwd_loss(pred, target, weight, eps=1e-6):
     sigma2_2 = w2 / 2 * torch.sin(theta2) * torch.cos(theta2) - h2 / 2 * torch.sin(theta2) * torch.cos(theta2)
     sigma2_3 = w2 / 2 * torch.sin(theta2) * torch.cos(theta2) - h2 / 2 * torch.sin(theta2) * torch.cos(theta2)
     sigma2_4 = w2 / 2 * torch.sin(theta2) ** 2 + h2 / 2 * torch.cos(theta2) ** 2
-    sigma2 = torch.reshape(torch.cat((sigma2_1.unsqueeze(1), sigma2_2.unsqueeze(1), sigma2_3.unsqueeze(1), sigma2_4.unsqueeze(1)), axis=1), (-1, 2, 2))
+    sigma2 = torch.reshape(
+        torch.cat((sigma2_1.unsqueeze(1), sigma2_2.unsqueeze(1), sigma2_3.unsqueeze(1), sigma2_4.unsqueeze(1)), axis=1),
+        (-1, 2, 2))
 
     wasserstein_distance_item1 = (x1 - x2) ** 2 + (y1 - y2) ** 2
     wasserstein_distance_item2 = wasserstein_distance_sigma(sigma1, sigma2)
-    wasserstein_distance = torch.max(wasserstein_distance_item1 + wasserstein_distance_item2, Variable(torch.zeros(wasserstein_distance_item1.shape[0]).type(torch.FloatTensor),requires_grad=False))
-    # wasserstein_distance = torch.max(torch.log(wasserstein_distance + 1 + eps), Variable(torch.zeros(wasserstein_distance_item1.shape[0]).type(torch.FloatTensor),requires_grad=False))
-    wasserstein_distance = torch.max(torch.sqrt(wasserstein_distance + eps), Variable(torch.zeros(wasserstein_distance_item1.shape[0]).type(torch.FloatTensor),requires_grad=False))
+    wasserstein_distance = torch.max(wasserstein_distance_item1 + wasserstein_distance_item2,
+                                     Variable(torch.zeros(wasserstein_distance_item1.shape[0]).type(torch.FloatTensor),
+                                              requires_grad=False))
+    wasserstein_distance = torch.max(torch.sqrt(wasserstein_distance + eps),
+                                     Variable(torch.zeros(wasserstein_distance_item1.shape[0]).type(torch.FloatTensor),
+                                              requires_grad=False))
     wasserstein_similarity = 1 / (wasserstein_distance + 2)
     wasserstein_loss = 1 - wasserstein_similarity
-    
-    return wasserstein_loss.mean()
+
+    return wasserstein_loss
 
 
-# @LOSSES.register_module()
-# class GWDLoss(nn.Module):
+def xywhr2xyrs(xywhr):
+    xywhr = xywhr.reshape(-1, 5)
+    xy = xywhr[..., :2]
+    wh = xywhr[..., 2:4].clamp(min=1e-7, max=1e7)
+    r = xywhr[..., 4]
+    cos_r = torch.cos(r)
+    sin_r = torch.sin(r)
+    R = torch.stack((cos_r, -sin_r, sin_r, cos_r), dim=-1).reshape(-1, 2, 2)
+    S = 0.5 * torch.diag_embed(wh)
+    return xy, R, S
 
-#     def __init__(self, eps=1e-6, reduction='mean', loss_weight=1.0):
-#         super(GWDLoss, self).__init__()
-#         self.eps = eps
-#         self.reduction = reduction
-#         self.loss_weight = loss_weight
 
-#     def forward(self,
-#                 pred,
-#                 target,
-#                 weight=None,
-#                 avg_factor=None,
-#                 reduction_override=None,
-#                 **kwargs):
-#         assert reduction_override in (None, 'none', 'mean', 'sum')
-#         reduction = (
-#             reduction_override if reduction_override else self.reduction)
-#         if (weight is not None) and (not torch.any(weight > 0)) and (
-#                 reduction != 'none'):
-#             return (pred * weight).sum()  # 0
-#         if weight is not None and weight.dim() > 1:
-#             assert weight.shape == pred.shape
-#             weight = weight.mean(-1)
-#         loss = self.loss_weight * gwd_loss(
-#             pred,
-#             target,
-#             weight,
-#             eps=self.eps,
-#             # reduction=reduction,
-#             # avg_factor=avg_factor,
-#             **kwargs)
-#         return loss
+def gwd_loss(pred, target, fun='log', tau=1.0, alpha=1.0, normalize=False):
+    """
+    given any positive-definite symmetrical 2*2 matrix Z:
+    Tr(Z^(1/2)) = sqrt(λ_1) + sqrt(λ_2)
+    where λ_1 and λ_2 are the eigen values of Z
 
+    meanwhile we have:
+    Tr(Z) = λ_1 + λ_2
+    det(Z) = λ_1 * λ_2
+
+    combination with following formula:
+    (sqrt(λ_1) + sqrt(λ_2))^2 = λ_1 + λ_2 + 2 * sqrt(λ_1 * λ_2)
+
+    yield:
+    Tr(Z^(1/2)) = sqrt(Tr(Z) + 2 * sqrt(det(Z)))
+
+    for gwd loss the frustrating coupling part is:
+    Tr((Σp^(1/2) * Σt * Σp^(1/2))^(1/2))
+
+    assuming Z = Σp^(1/2) * Σt * Σp^(1/2) then:
+    Tr(Z) = Tr(Σp^(1/2) * Σt * Σp^(1/2))
+    = Tr(Σp^(1/2) * Σp^(1/2) * Σt)
+    = Tr(Σp * Σt)
+    det(Z) = det(Σp^(1/2) * Σt * Σp^(1/2))
+    = det(Σp^(1/2)) * det(Σt) * det(Σp^(1/2))
+    = det(Σp * Σt)
+
+    and thus we can rewrite the coupling part as:
+    Tr((Σp^(1/2) * Σt * Σp^(1/2))^(1/2))
+    = Tr{Z^(1/2)} = sqrt(Tr(Z) + 2 * sqrt(det(Z))
+    = sqrt(Tr(Σp * Σt) + 2 * sqrt(det(Σp * Σt)))
+    """
+    xy_p, R_p, S_p = xywhr2xyrs(pred)
+    xy_t, R_t, S_t = xywhr2xyrs(target)
+
+    xy_distance = (xy_p - xy_t).square().sum(dim=-1)
+
+    Sigma_p = R_p.matmul(S_p.square()).matmul(R_p.permute(0, 2, 1))
+    Sigma_t = R_t.matmul(S_t.square()).matmul(R_t.permute(0, 2, 1))
+
+    whr_distance = S_p.diagonal(dim1=-2, dim2=-1).square().sum(dim=-1)
+    whr_distance = whr_distance + S_t.diagonal(dim1=-2, dim2=-1).square().sum(
+        dim=-1)
+    _t = Sigma_p.matmul(Sigma_t)
+
+    _t_tr = _t.diagonal(dim1=-2, dim2=-1).sum(dim=-1)
+    _t_det_sqrt = S_p.diagonal(dim1=-2, dim2=-1).prod(dim=-1)
+    _t_det_sqrt = _t_det_sqrt * S_t.diagonal(dim1=-2, dim2=-1).prod(dim=-1)
+    whr_distance = whr_distance + (-2) * (
+        (_t_tr + 2 * _t_det_sqrt).clamp(0).sqrt())
+
+    distance = (xy_distance + alpha * alpha * whr_distance).clamp(0).sqrt()
+
+    if normalize:
+        wh_p = pred[..., 2:4].clamp(min=1e-7, max=1e7)
+        wh_t = target[..., 2:4].clamp(min=1e-7, max=1e7)
+        scale = ((wh_p.log() + wh_t.log()).sum(dim=-1) / 4).exp()
+        distance = distance / scale
+    return distance
+
+    # if fun == 'log':
+    #     distance = torch.log1p(distance)
+    # elif fun == 'none':
+    #     pass
+    # else:
+    #     raise ValueError('Invalid non-linear function {fun} for gwd loss')
+    #
+    # if tau >= 1.0:
+    #     return 1 - 1 / (tau + distance)
+    # else:
+    #     return distance
+
+    # @LOSSES.register_module()
+    # class GWDLoss(nn.Module):
+
+    #     def __init__(self, eps=1e-6, reduction='mean', loss_weight=1.0):
+    #         super(GWDLoss, self).__init__()
+    #         self.eps = eps
+    #         self.reduction = reduction
+    #         self.loss_weight = loss_weight
+
+    #     def forward(self,
+    #                 pred,
+    #                 target,
+    #                 weight=None,
+    #                 avg_factor=None,
+    #                 reduction_override=None,
+    #                 **kwargs):
+    #         assert reduction_override in (None, 'none', 'mean', 'sum')
+    #         reduction = (
+    #             reduction_override if reduction_override else self.reduction)
+    #         if (weight is not None) and (not torch.any(weight > 0)) and (
+    #                 reduction != 'none'):
+    #             return (pred * weight).sum()  # 0
+    #         if weight is not None and weight.dim() > 1:
+    #             assert weight.shape == pred.shape
+    #             weight = weight.mean(-1)
+    #         loss = self.loss_weight * gwd_loss(
+    #             pred,
+    #             target,
+    #             weight,
+    #             eps=self.eps,
+    #             # reduction=reduction,
+    #             # avg_factor=avg_factor,
+    #             **kwargs)
+    #         return loss
 
 if __name__ == '__main__':
+    # pred = torch.FloatTensor([[50, 50, 10, 70, -30 / 180 * np.pi],
+    #                           [50, 50, 10, 70, -30 / 180 * np.pi]])
+    # target = torch.FloatTensor([[50, 50, 10, 70, -60 / 180 * np.pi],
+    #                             [50, 50, 10, 40, -30 / 180 * np.pi]])
 
     pred = torch.FloatTensor([[50, 50, 10, 70, -30 / 180 * np.pi],
-                              [50, 50, 10, 70, -30 / 180 * np.pi]])
-    target = torch.FloatTensor([[50, 50, 10, 70, -60 / 180 * np.pi],
-                                [50, 50, 10, 40, -30 / 180 * np.pi]])
+                              [50, 50, 100, 700, -30 / 180 * np.pi]])
+    target = torch.FloatTensor([[10, 40, 10, 70, -30 / 180 * np.pi],
+                                [10, 40, 100, 700, -40 / 180 * np.pi]])
 
     weight = torch.FloatTensor([1.0, 1.0])
 
-    print(gwd_loss(pred, target, weight, 1e-6))
-
-
-
+    print(gwd_loss(pred, target))
